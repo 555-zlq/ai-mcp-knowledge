8be51bf31c5db025410af62894642d41672f6cde
carton <2181058263@qq.com>
2025-12-09 20:20:09 +0800

    init: 初始化工程

--- docs/dev-ops/api/curl_yun.sh
+++ docs/dev-ops/api/curl_yun.sh
@@ -1,4 +1,4 @@
-curl http://58a39caa684c41bf9bed-deepseek-r1-llm-api.gcs-xy1a.jdcloud.com/api/generate \
+curl http://da04e4a739ea4f1f9a93-deepseek-r1-llm-api.gcs-xy1a.jdcloud.com/api/generate \
   -H "Content-Type: application/json" \
   -d '{
         "model": "deepseek-r1:1.5b",
